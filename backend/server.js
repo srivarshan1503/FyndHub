5593670cab72fcdeea16b41b6643945b235f3ec0
srivarshan1503 <srivarshan1503@gmail>
2026-07-11 18:50:22 +0530

    fix: add Vercel URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const cors = require('cors')
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://fyndhub.vercel.app'],
     credentials: true
 }))
 
