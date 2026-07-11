27abeb345c1b9dd901a8d68f235851cf320a178c
srivarshan1503 <srivarshan1503@gmail>
2026-07-11 17:44:18 +0530

    commit 4

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false },
+    family: 4
 })
 
 module.exports = pool
