27e958f3ba64e48a8b8dd8726b63639000da739f
srivarshan1503 <srivarshan1503@gmail>
2026-07-11 18:23:57 +0530

    update API URL to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
-});
+    baseURL: 'https://fyndhub.onrender.com/api'
+})
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem("token");
